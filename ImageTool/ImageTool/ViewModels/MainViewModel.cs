74edbaf9900a5f8fb57a7d818ace0dfa53312679
DoruApareci <93883236+DoruApareci@users.noreply.github.com>
2022-11-09 12:46:46 +0200

    main view, viewmodel, main window

---
+++ ImageTool/ImageTool/ViewModels/MainViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageTool.ViewModels
+{
+    internal class MainViewModel:BaseViewModel
+    {
+        //BitmapImage _image
+        public int Progress { get; set; } = 50;
+        public string StatusText { get; set; } = "Ready";
+        public Bitmap Image { get; set; } = new Bitmap(100, 100);
+    }
+}
