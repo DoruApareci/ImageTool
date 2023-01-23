34f5a9930af517d006d5b7601cdff39f0156fa62
DoruApareci <93883236+DoruApareci@users.noreply.github.com>
2023-01-23 04:05:59 +0200

    Open File Command, need to work on SaveFileCommand

--- ImageTool/ImageTool/Commands/OpenFileCommand.cs
+++ ImageTool/ImageTool/Commands/OpenFileCommand.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using PropertyChanged;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,14 +11,26 @@ namespace ImageTool.Commands
 {
     public class OpenFileCommand : BaseCommand
     {
+        public string FilePath { get; private set; } = string.Empty;
+        
         public override bool CanExecute(object? parameter)
         {
-            throw new NotImplementedException();
+            return true;
         }
 
         public override void Execute(object? parameter)
         {
-            throw new NotImplementedException();
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Images (*.png;*.jpg;*.bmp)|*.png;*.jpg;*.bmp"; 
+            if (openFileDialog.ShowDialog() == true)
+            {
+                FilePath = openFileDialog.FileName;
+                if (!File.Exists(FilePath))
+                {
+                    FilePath = string.Empty;
+                    throw new FileNotFoundException("File not found");
+                }
+            }
         }
     }
 }
--- ImageTool/ImageTool/Commands/SaveFileCommand.cs
+++ ImageTool/ImageTool/Commands/SaveFileCommand.cs
@@ -6,7 +6,18 @@ using System.Threading.Tasks;
 
 namespace ImageTool.Commands
 {
-    public class SaveFileCommand
+    public class SaveFileCommand : BaseCommand
     {
+        public override bool CanExecute(object? parameter)
+        {
+            //check if file is loaded
+            return true;
+        }
+
+        public override void Execute(object? parameter)
+        {
+            //save file dialog with image filter
+            //and export/save image
+        }
     }
 }
