3a0a3577a8a0bc5811c2ad71e284da4054a2f408
DoruApareci <93883236+DoruApareci@users.noreply.github.com>
2023-01-24 07:28:49 +0200

    NavService

---
+++ ImageTool/ImageTool/Services/NavigationService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace ImageTool.Services
+{
+    public class NavigationService
+    {
+        public NavigationService(MainWindow window)
+        {
+            MainWindow = window;
+        }
+
+        public MainWindow MainWindow { get; set; }
+
+        public void Navigate(UserControl view)
+        {
+            MainWindow.UpdateView(view);
+        }
+    }
+}
